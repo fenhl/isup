7fc64847ed45229220e9bdfe20c25f3c83f10a80
Max Dominik Weber <fenhl@fenhl.net>
2013-04-02 15:28:28 +0000

    Add isup.py
    This file is a modified version of https://gist.github.com/andrix/1423960#file-isup-py

---
+++ isup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import re
+import sys
+from urllib.request import urlopen
+
+def isup(domain):
+    request = urlopen("http://www.isup.me/" + domain).read()
+    if type(request) != type(''):
+        request = request.decode('utf-8')
+    return domain + " " + ("UP" if "It's just you" in request else "DOWN")
+
+def main(cmd, args):
+    if len(args):
+        for d in args:
+            print(isup(d))
+    else:
+        print("usage: " + cmd + " domain1 [domain2 .. domainN]")
+
+if __name__ == '__main__':
+    main(sys.argv[0], sys.argv[1:] if len(sys.argv) > 1 else [])
