70cb55d47b34335b8e63404ad04b82e081e984e9
Fenhl <fenhl@fenhl.net>
2013-10-02 14:23:05 +0000

    Code and readme overhaul
    includes nicer output formatting and semver

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-Overview
-========
+**isup** is a Python 3 script that uses [isup.me][isupme] to check if a website is up or down, based on [this Python 2 script][gistisup].
 
-A Python 3 script that uses [isup.me][isupme] to check if a website is up or down, based on [this Python 2 script][gistisup].
+This is `isup` version 1.0.0 ([semver][]). The versioned API includes the usage pattern, as found in the docstring of [`isup.py`](isup.py).
 
-[gistisup]: https://gist.github.com/andrix/1423960#file-isup-py (gist:andrix/isup.py)
-[isupme]: http://isup.me/ (isup.me)
+Requirements
+============
+
+To run `isup`, you will need [docopt][].
 
 Changes
 =======
@@ -13,3 +14,8 @@ The following things have been changed:
 
 *   The script has been updated to Python 3.
 *   When checking multiple websites at once, the script now prints the result for each website before checking the next one.
+
+[docopt]: http://docopt.org/ (docopt)
+[gistisup]: https://gist.github.com/andrix/1423960#file-isup-py (gist:andrix/isup.py)
+[isupme]: http://isup.me/ (isup.me)
+[semver]: http://semver.org/ (Semantic Versioning 2.0.0)
--- isup.py
+++ isup.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
+
+"""Check whether a domain is up using isup.me
+
+Usage:
+  isup <domain>...
+  isup -h | --help
+  isup --version
+
+Options:
+  -h, --help  
+"""
+
+from docopt import docopt
 import re
 import sys
 from urllib.request import urlopen
 
 def isup(domain):
-    request = urlopen("http://www.isup.me/" + domain).read()
-    if type(request) != type(''):
-        request = request.decode('utf-8')
-    return domain + " " + ("UP" if "It's just you" in request else "DOWN")
+    try:
+        request = urlopen("http://www.isup.me/" + domain).read()
+        if type(request) != type(''):
+            request = request.decode('utf-8')
+        return bool("It's just you" in request)
+    except:
+        return False
 
-def main(cmd, args):
-    if len(args):
-        for d in args:
-            print(isup(d))
-    else:
-        print("usage: " + cmd + " domain1 [domain2 .. domainN]")
+def main(domains):
+    for domain in domains:
+        print('[....] ' + domain, end='\r')
+        print('[ UP ]' if isup(domain) else '[DOWN]')
 
 if __name__ == '__main__':
-    main(sys.argv[0], sys.argv[1:] if len(sys.argv) > 1 else [])
+    arguments = docopt(__doc__, version='isup 1.0.0')
+    main(arguments['<domain>'])
